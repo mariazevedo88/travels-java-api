acb893784899e9bd99a79660f7bef53e9b5d7286
Mariana Azevedo <mariana@bsi.ufla.br>
2019-09-15 17:02:43 -0300

    Javadoc the transaction factory package.

--- src/main/java/io/github/mariazevedo88/financialjavaapi/factory/TransactionFactory.java
+++ src/main/java/io/github/mariazevedo88/financialjavaapi/factory/TransactionFactory.java
@@ -2,6 +2,12 @@ package io.github.mariazevedo88.financialjavaapi.factory;
 
 import io.github.mariazevedo88.financialjavaapi.model.Transaction;
 
+/**
+ * Interface that provides method for manipulate a transaction.
+ * 
+ * @author Mariana Azevedo
+ * @since 08/09/2019
+ */
 public interface TransactionFactory {
 
 	Transaction createTransaction(String type);
--- src/main/java/io/github/mariazevedo88/financialjavaapi/factory/impl/TransactionFactoryImpl.java
+++ src/main/java/io/github/mariazevedo88/financialjavaapi/factory/impl/TransactionFactoryImpl.java
@@ -4,6 +4,12 @@ import io.github.mariazevedo88.financialjavaapi.enumeration.TransactionTypeEnum;
 import io.github.mariazevedo88.financialjavaapi.factory.TransactionFactory;
 import io.github.mariazevedo88.financialjavaapi.model.Transaction;
 
+/**
+ * Factory class for the Transaction entity.
+ * 
+ * @author Mariana Azevedo
+ * @since 08/09/2019
+ */
 public class TransactionFactoryImpl implements TransactionFactory{
 
 	@Override
