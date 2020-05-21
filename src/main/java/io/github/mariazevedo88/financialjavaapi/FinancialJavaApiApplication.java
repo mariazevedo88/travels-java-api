59d377e369ea4cc2fbc00d27f2600f9586b52eaa
Mariana Azevedo <mariana@bsi.ufla.br>
2020-05-21 14:47:09 -0300

    Correcting application class.

--- src/main/java/io/github/mariazevedo88/financialjavaapi/FinancialJavaApiApplication.java
+++ src/main/java/io/github/mariazevedo88/financialjavaapi/FinancialJavaApiApplication.java
@@ -2,6 +2,7 @@ package io.github.mariazevedo88.financialjavaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
